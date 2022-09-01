4607d0c707f9e8a27f88d9f35fd22d66da2f2bfc
AllardKamiel <61234087+AllardKamiel@users.noreply.github.com>
2022-09-01 15:42:07 +0200

    Add files via upload

---
+++ network_start.bash
@@ -0,0 +1,23 @@
+cd network
+NODE_URL='http://192.168.122.98:14265'
+echo $NODE_URL
+cd author/author_data
+AUTH_DIR_PATH=$(realpath .)
+echo $AUTH_DIR_PATH
+
+cd ../..
+
+
+for (( c=1; c<=$1; c++ ))
+do 
+    cd subscriber$c/subscriber_data
+    SUB_DIR_PATH=$(realpath .)
+    cd ../subscriber_operation/build
+    cmake ../code
+    cmake --build .
+    gnome-terminal -- bash -c "./Tutorial "$AUTH_DIR_PATH" "$SUB_DIR_PATH" "$NODE_URL"; exec bash"
+    
+    cd ../../..
+done
+
+
